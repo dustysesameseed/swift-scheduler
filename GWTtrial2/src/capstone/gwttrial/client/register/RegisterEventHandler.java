89bda24cf994042a3a725dce7ba48faa8e28e31b
dustysesameseed <dustysesameseed@gmail.com>
2015-11-16 15:36:27 -0500

    add register functionality

---
+++ GWTtrial2/src/capstone/gwttrial/client/register/RegisterEventHandler.java
@@ -0,0 +1,8 @@
+package capstone.gwttrial.client.register;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface RegisterEventHandler extends EventHandler {
+
+	void onRegister(RegisterEvent registerEvent);
+}
